2beb480a5cace1933747b99521cf402d49c4eb78
Christoph Witzko <github@christophwitzko.com>
2022-08-07 16:11:24 +0200

    feat: add git package

--- pkg/benchmark/setup/setup.go
+++ pkg/benchmark/setup/setup.go
@@ -1,62 +1,13 @@
 package setup
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"path"
 
+	"github.com/christophwitzko/master-thesis/pkg/git"
 	"github.com/christophwitzko/master-thesis/pkg/logger"
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
-	cp "github.com/otiai10/copy"
 )
-
-func checkRefIfExists(refType string, repo *git.Repository, tagName string) (bool, error) {
-	var refName plumbing.ReferenceName
-	switch refType {
-	case "tag":
-		refName = plumbing.NewTagReferenceName(tagName)
-	case "branch":
-		refName = plumbing.NewBranchReferenceName(tagName)
-	default:
-		return false, fmt.Errorf("unknown ref type: %s", refType)
-	}
-	_, err := repo.Reference(refName, false)
-	if err != nil {
-		if errors.Is(err, plumbing.ErrReferenceNotFound) {
-			return false, nil
-		}
-		return false, err
-	}
-	return true, nil
-}
-
-func checkoutRef(repo *git.Repository, refName string) error {
-	repoTree, err := repo.Worktree()
-	if err != nil {
-		return err
-	}
-	checkoutOptions := &git.CheckoutOptions{}
-	isTag, err := checkRefIfExists("tag", repo, refName)
-	if err != nil {
-		return err
-	}
-	if isTag {
-		checkoutOptions.Branch = plumbing.NewTagReferenceName(refName)
-		return repoTree.Checkout(checkoutOptions)
-	}
-	isBranch, err := checkRefIfExists("branch", repo, refName)
-	if err != nil {
-		return err
-	}
-	if isBranch {
-		checkoutOptions.Branch = plumbing.NewBranchReferenceName(refName)
-	} else {
-		checkoutOptions.Hash = plumbing.NewHash(refName)
-	}
-	return repoTree.Checkout(checkoutOptions)
-}
 
 func SourcePathsFromGitRepository(log *logger.Logger, benchDir, repoURL, refV1, refV2 string) (string, string, error) {
 	if err := os.RemoveAll(benchDir); err != nil {
@@ -68,31 +19,18 @@ func SourcePathsFromGitRepository(log *logger.Logger, benchDir, repoURL, refV1, 
 
 	sourcePathV1 := path.Join(benchDir, "v1")
 	sourcePathV2 := path.Join(benchDir, "v2")
-	log.Infof("cloning %s to %s", repoURL, sourcePathV1)
-	repoV1, err := git.PlainClone(sourcePathV1, false, &git.CloneOptions{
-		URL:  repoURL,
-		Tags: git.AllTags,
-	})
+	log.Infof("cloning %s to %s and %s", repoURL, sourcePathV1, sourcePathV2)
+	repos, err := git.Clone(repoURL, sourcePathV1, sourcePathV2)
 	if err != nil {
-		return "", "", fmt.Errorf("failed to clone %s to %s: %w", repoURL, sourcePathV1, err)
-	}
-
-	log.Infof("duplicating repository to %s", sourcePathV2)
-	err = cp.Copy(sourcePathV1, sourcePathV2)
-	if err != nil {
-		return "", "", fmt.Errorf("failed to copy %s to %s: %w", sourcePathV1, sourcePathV2, err)
-	}
-	repoV2, err := git.PlainOpen(sourcePathV2)
-	if err != nil {
-		return "", "", fmt.Errorf("failed to open %s: %w", sourcePathV1, err)
+		return "", "", fmt.Errorf("failed to clone %s: %w", repoURL, err)
 	}
 
 	log.Infof("checking out v1: %s", refV1)
-	if err := checkoutRef(repoV1, refV1); err != nil {
+	if err := git.CheckoutReference(repos[0], refV1); err != nil {
 		return "", "", err
 	}
 	log.Infof("checking out v2: %s", refV2)
-	if err := checkoutRef(repoV2, refV2); err != nil {
+	if err := git.CheckoutReference(repos[1], refV2); err != nil {
 		return "", "", err
 	}
 	return sourcePathV1, sourcePathV2, nil
---
+++ pkg/git/git.go
@@ -0,0 +1,93 @@
+package git
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	cp "github.com/otiai10/copy"
+)
+
+type referenceType string
+
+const (
+	referenceTypeTag    = referenceType("tag")
+	referenceTypeBranch = referenceType("branch")
+)
+
+func checkRefIfExists(refType referenceType, repo *git.Repository, tagName string) (bool, error) {
+	var refName plumbing.ReferenceName
+	switch refType {
+	case referenceTypeTag:
+		refName = plumbing.NewTagReferenceName(tagName)
+	case referenceTypeBranch:
+		refName = plumbing.NewBranchReferenceName(tagName)
+	default:
+		return false, fmt.Errorf("unknown ref type: %s", refType)
+	}
+	_, err := repo.Reference(refName, false)
+	if err != nil {
+		if errors.Is(err, plumbing.ErrReferenceNotFound) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func CheckoutReference(repo *git.Repository, refName string) error {
+	repoTree, err := repo.Worktree()
+	if err != nil {
+		return err
+	}
+	checkoutOptions := &git.CheckoutOptions{}
+	isTag, err := checkRefIfExists(referenceTypeTag, repo, refName)
+	if err != nil {
+		return err
+	}
+	if isTag {
+		checkoutOptions.Branch = plumbing.NewTagReferenceName(refName)
+		return repoTree.Checkout(checkoutOptions)
+	}
+	isBranch, err := checkRefIfExists(referenceTypeBranch, repo, refName)
+	if err != nil {
+		return err
+	}
+	if isBranch {
+		checkoutOptions.Branch = plumbing.NewBranchReferenceName(refName)
+	} else {
+		checkoutOptions.Hash = plumbing.NewHash(refName)
+	}
+	return repoTree.Checkout(checkoutOptions)
+}
+
+// Clone clones the given repository to the provided paths.
+func Clone(repoURL string, destinationDirs ...string) ([]*git.Repository, error) {
+	if len(destinationDirs) == 0 {
+		return nil, errors.New("no destination directory specified")
+	}
+
+	mainRepoDir := destinationDirs[0]
+	repo, err := git.PlainClone(mainRepoDir, false, &git.CloneOptions{
+		URL:  repoURL,
+		Tags: git.AllTags,
+	})
+	if err != nil {
+		return nil, err
+	}
+	repos := make([]*git.Repository, len(destinationDirs))
+	repos[0] = repo
+	for i, destDir := range destinationDirs[1:] {
+		err := cp.Copy(mainRepoDir, destDir)
+		if err != nil {
+			return nil, err
+		}
+		repo, err := git.PlainOpen(destDir)
+		if err != nil {
+			return nil, err
+		}
+		repos[i+1] = repo
+	}
+	return repos, nil
+}
