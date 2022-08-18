15da34bc28036b278099d387f5794e7dab8f738e
Christoph Witzko <github@christophwitzko.com>
2022-08-18 14:05:45 +0200

    fix: simulate run script

--- scripts/simulate-run.sh
+++ scripts/simulate-run.sh
@@ -2,7 +2,14 @@
 
 set -euo pipefail
 
-
+rm -f results.csv
 for i in {1..3} ; do
-  ./microbenchmark-runner -i ./bench-fns.json -r "$i" >> results.csv
+  ./microbenchmark-runner \
+    --v1 main \
+    --v2 perf-issue-request-id \
+    --git-repository='https://github.com/christophwitzko/flight-booking-service.git' \
+    --exclude-filter="^chi.*$" \
+    --include-filter="^service.*$" \
+    -o "-?no-csv-header=true" \
+    --run "$i" >> results.csv
 done
