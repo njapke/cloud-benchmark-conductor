8ee676bebd743961015b84a3dbe61469ad765e84
Christoph Witzko <github@christophwitzko.com>
2022-08-31 17:15:11 +0200

    feat: add build and run on linux script

---
+++ scripts/build-and-run-on-linux.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "building..."
+export CGO_ENABLED=0
+GOOS=linux GOARCH=arm64 go build -trimpath -ldflags="-s -w -extldflags '-static'" -o bin-linux-arm64 "$1"
+
+echo "uploading..."
+scp bin-linux-arm64 chris@192.168.64.4:~/bin-linux-arm64
+
+shift
+echo "running with '$*'"
+function sshkill() {
+  echo "sending SIGINT"
+  ssh chris@192.168.64.4 'kill -s SIGTERM $(pidof ./bin-linux-arm64)'
+}
+trap sshkill SIGINT
+ssh -t chris@192.168.64.4 "./bin-linux-arm64 $*" &
+while true; do sleep 1; done
